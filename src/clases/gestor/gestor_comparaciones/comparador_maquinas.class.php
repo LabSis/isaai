e2e108307ebc4f5d5346c8cfe10542e44f075780
Milagros Zea <Milagros Zea@172.16.170.17>
2014-10-07 13:16:32 -0300

    Finalización de clase ComparadorMaquinas

--- src/clases/gestor/gestor_comparaciones/comparador_maquinas.class.php
+++ src/clases/gestor/gestor_comparaciones/comparador_maquinas.class.php
@@ -28,17 +28,36 @@ class ComparadorMaquinas {
         /* Comparar las dos maquinas 1 y 2. y dejar los cambios en el array componentes_cambiados. */
         $iguales = true;
 
-//        if ($maquina1->get_bios()->equals($maquina2->get_bios()) === false) {
-//            $this->_componentes_cambiados[] = $maquina2->get_bios();
-//            $iguales = false;
-//        }
-        for ($i = 0; $i < count($maquina1->get_procesadores()); $i++) {
-            if(!$maquina1->get_procesador($i)->equals($maquina2->get_procesador($i))){
-                $this->_componentes_cambiados[] = $maquina1->get_procesador($i);
-                $iguales = false;
+        if (!$maquina1->get_bios()->equals($maquina2->get_bios())) {
+            $this->_componentes_cambiados[] = $maquina2->get_bios();
+            $iguales = false;
+        }
+//		for ($i = 0; i < count($maquina1->get_placas_video()); $i++){
+//			if(!$maquina1->get_placas_video($i)->equals($maquina2->get_placas_video($i))){
+//				$this->_componentes_cambiados[] = $maquina1->get_placas_video($i);
+//				$iguales= false;
+//			}
+//		}
+        $iguales = $this->verificar_cambios($maquina1->get_discos(), $maquina2->get_discos());
+        $iguales = $this->verificar_cambios($maquina1->get_memorias(), $maquina2->get_memorias());
+        $iguales = $this->verificar_cambios($maquina1->get_monitores(), $maquina2->get_monitores());
+        $iguales = $this->verificar_cambios($maquina1->get_perifericos(), $maquina2->get_perifericos());
+        $iguales = $this->verificar_cambios($maquina1->get_placas_red(), $maquina2->get_placas_red());
+        $iguales = $this->verificar_cambios($maquina1->get_placas_sonido(), $maquina2->get_placas_sonido());
+        $iguales = $this->verificar_cambios($maquina1->get_placas_video(), $maquina2->get_placas_video());
+        $iguales = $this->verificar_cambios($maquina1->get_procesadores(), $maquina2->get_procesadores());
+        return $iguales;
+    }
+
+    private function verificar_cambios($maquina1_componentes_tipados, $maquina2_componentes_tipados) {
+        $igual = true;
+        for ($i = 0; $i < count($maquina1_componentes_tipados()); $i++) {
+            if (!$maquina1_componentes_tipados($i)->equals($maquina2_componentes_tipados($i))) {
+                $this->_componentes_cambiados[] = $maquina1_componentes_tipados($i);
+                $igual = false;
             }
         }
-        return $iguales;
+        return $igual;
     }
 
     public function get_componentes_cambiados() {
