a8464b758a7aac33b22669fc0b962c077ad0b94c
Milagros Zea <Milagros Zea@172.16.170.8>
2014-11-18 11:04:21 -0300

    Modificación método alertar del gesto comunicaciones

--- src/clases/gestor/gestor_comunicaciones/gestor_comunicaciones.class.php
+++ src/clases/gestor/gestor_comunicaciones/gestor_comunicaciones.class.php
@@ -14,22 +14,32 @@ class GestorComunicaciones {
      * @param array de \Alertador Es un alertador.
      */
     public function alertar($lista_cambios, $alertadores) {
-        for($i = 0; $i < count($lista_cambios); $i++){
-            for($j = 0; $j < count($alertadores); $j++){
+        for ($i = 0; $i < count($lista_cambios); $i++) {
+            for ($j = 0; $j < count($alertadores); $j++) {
                 //array tipo de cambios
-                $tipo_cambios = $this->determinar_tipo_cambio($lista_cambios[$i]);
-                //deberia agregar por cada rol mas de u tipo de cambio
-                $roles = $this->determinar_roles_a_enviar($tipo_cambio);
-                $alertadores[$j]->alertar($lista_cambios[$i], $roles);
+                $tipos_cambios = $this->determinar_tipo_cambio($lista_cambios[$i]);
+                //deberia agregar por cada tipo de cambio mas de un rol
+                $roles_comunicar = array();
+                foreach ($tipos_cambios as $tipo_cambio) {
+                    $roles_actuales = $this->determinar_roles_a_enviar($tipo_cambio);
+                    foreach ($roles_actuales as $rol_actual) {
+                        if (!in_array($rol_actual, $roles_comunicar, true)) {
+                            $roles_comunicar[] = $rol_actual;
+                        }
+                    }
+                    $alertadores[$j]->alertar($lista_cambios[$i], $roles);
+                }
+                
             }
         }
     }
+
     /**
      * Retorna un tipo de cambio a partir de un cambio.
      * @param \Cambio $cambio
      * @return \TipoCambio
      */
-    public function determinar_tipo_cambio($cambio){
+    public function determinar_tipo_cambio($cambio) {
         //Suponemos que lo que haces es identificar los compoenntes que cambiaron,
         //en base a eso y a consutlar la tabla tipos_cambios, saber que instancia
         //de tipo de cambio devolver?
@@ -41,7 +51,8 @@ class GestorComunicaciones {
      * @param \TipoCambio $tipo_cambio
      * @return Array de \Rol
      */
-    public function determinar_roles_a_enviar($tipo_cambio){
+    public function determinar_roles_a_enviar($tipo_cambio) {
         return Rol::determinar_roles_a_enviar($tipo_cambio);
     }
+
 }
