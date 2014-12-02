450d032cf8f252c19c74aca533c5f02682456fd1
Diego <Diego@DreamBuilder>
2014-12-02 16:12:59 -0300

    Menu principal scalable.

--- js/maquetado.js
+++ js/maquetado.js
@@ -8,26 +8,46 @@ $(document).ready(function() {
             $("#menuSeccionUsuario").css("opacity", "0.0");
         }
     });
+    //btn ocultar mostrar menu
+    $("#seccionOcultarMostrarMenu").click(function() {
+        if ($("#menuPrincipal").hasClass("menuOculto")) {
+            //mostrar
+            $("#contenido").css("width", "85%");
+            $("#menuPrincipal").css("width", "15%");
+            $("#menuPrincipal .spnTextMenu").show();
+            $("#menuPrincipal").removeClass("menuOculto");
+            $(this).children(".icono").removeClass("fa-arrow-right");
+            $(this).children(".icono").addClass("fa-arrow-left");
+        } else {
+            //ocultar
+            $("#contenido").css("width", "97%");
+            $("#menuPrincipal").css("width", "3%");
+            $("#menuPrincipal .spnTextMenu").hide();
+            $("#menuPrincipal").addClass("menuOculto");
+            $(this).children(".icono").removeClass("fa-arrow-left");
+            $(this).children(".icono").addClass("fa-arrow-right");
+        }
+    });
     $("html").click(function() {
         if (parseInt($("#menuSeccionUsuario").css("opacity")) > 0) {
             $("#menuSeccionUsuario").css("top", "45px");
             $("#menuSeccionUsuario").css("opacity", "0.0");
         }
         /*
-        if (parseInt($("#cabeceraMensajes").css("height")) != 0) {
-            $("#cabeceraMensajes").css("height", "0px");
-        } else {
-            $("#cabeceraMensajes").css("height", "30px");
-        }
-        */
+         if (parseInt($("#cabeceraMensajes").css("height")) != 0) {
+         $("#cabeceraMensajes").css("height", "0px");
+         } else {
+         $("#cabeceraMensajes").css("height", "30px");
+         }
+         */
     });
     //recalculo las dimensiones del maquetado
     resize();
 });
-$(window).resize(function(){
+$(window).resize(function() {
     resize();
 });
-function resize(){
+function resize() {
     $("#contenido").css("height", $(window).height() - (parseInt($("header").css("height")) + parseInt($("footer").css("height"))));
     $("#menuPrincipal").css("height", $(window).height() - (parseInt($("header").css("height")) + parseInt($("footer").css("height"))));
 }
--- tmpl/marco/menu_principal.tmpl.php
+++ tmpl/marco/menu_principal.tmpl.php
@@ -1,19 +1,19 @@
 <aside id="menuPrincipal">
     <ul>
         <li>
-            <a href="#" id="btnSincronizar"><i class="fa fa-refresh fa-fw icono"></i>Sincronizar</a>
+            <a href="#" id="btnSincronizar"><i class="fa fa-refresh fa-fw icono"></i><span class="spnTextMenu">Sincronizar</span></a>
         </li>
         <li>
-            <a href="<?php echo $global_ruta_web; ?>/src/ctrl/maquina/maquinas.ctrl.php"><i class="fa fa-desktop fa-fw icono"></i>Máquinas</a>
+            <a href="<?php echo $global_ruta_web; ?>/src/ctrl/maquina/maquinas.ctrl.php"><i class="fa fa-desktop fa-fw icono"></i><span class="spnTextMenu">Máquinas</span></a>
         </li>
         <li>
-            <a href="#"><i class="fa fa-cube fa-fw icono"></i>Componentes</a>
+            <a href="#"><i class="fa fa-cube fa-fw icono"></i><span class="spnTextMenu">Componentes</span></a>
         </li>
         <li>
-            <a href="#"><i class="fa fa-cog fa-fw icono"></i>Configurar</a>
+            <a href="#"><i class="fa fa-cog fa-fw icono"></i><span class="spnTextMenu">Configurar</span></a>
         </li>
     </ul>
-    <div id="btnOcultarMostrarMenu">
-        <i class="fa fa-arrow-right icono iconoBoton"></i>
+    <div id="seccionOcultarMostrarMenu">
+        <i class="fa fa-arrow-left icono iconoBoton"></i>
     </div>
 </aside>
