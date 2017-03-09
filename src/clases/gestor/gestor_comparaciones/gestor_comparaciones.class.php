6483c187c7f85c43fa3a247986d1d9339ef96bbe
Diego Barrionuevo <dsbarrionuevo@hotmail.com>
2017-03-09 16:37:59 -0300

    No muestro logs en ajax de sincronizar, asi muestro correctamente las cosas en la interfaz

--- src/clases/gestor/gestor_comparaciones/gestor_comparaciones.class.php
+++ src/clases/gestor/gestor_comparaciones/gestor_comparaciones.class.php
@@ -42,17 +42,12 @@ class GestorComparaciones {
         for ($i = 0; $i < count($maquinas_actuales); $i++) {
             $maquina_actual = $maquinas_actuales[$i];
             $maquina_anterior = $maquinas_anteriores[$i];
-//            Out::print_array($maquina_anterior);
-//            Out::print_array($maquina_actual);
             $comparador = new ComparadorMaquinas();
             if ($comparador->verificar_igualdad($maquina_actual, $maquina_anterior) == false) {
-                Out::println("NO SON IGUALES");
                 $cambio = new Cambio();
                 $cambio->set_maquina_anterior($maquina_anterior);
                 $cambio->set_maquina_actual($maquina_actual);
                 $cambio->set_componentes_cambiados($comparador->get_componentes_cambiados());
-                Out::println("Compo cambiados:");
-                Out::print_array($cambio->get_componentes_cambiados());
                 $cambios[] = $cambio;
                 //Actualizo las maquinas con los componentes actuales en la base de datos isaai
                 //Solo actualizo las fechas cuando el ocs este funcionando y tirando datos actules, es decir
@@ -60,7 +55,6 @@ class GestorComparaciones {
                 //$maquina_actual->set_fecha_cambio($fecha_cambio);
                 //$maquina_actual->set_fecha_sincronizacion($fecha_sincronizacion);
                 $exito = $maquina_actual->actualizar_cambios_componentes($comparador->get_componentes_cambiados());
-                Out::println("Exito al actualizar:" + $exito + ".");
                 //ACTUALIZO TODOS LOS COMPONENTES
                 //$maquina_actual->insertar();
             } else {
